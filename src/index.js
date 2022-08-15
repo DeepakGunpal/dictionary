c433f6513cfe6a85f59af53efe7a5478cc19c309
Deepak <deepakgunpal0@gmail.com>
2022-08-16 03:08:13 +0530

    half done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import route from './route/route.js';
+import path from "path";
+import pkg from 'dotenv';
+pkg.config({ path: './config.env' });
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -14,18 +17,12 @@ if (process.env.NODE_ENV == "production") {
 
     app.use(express.static("client/build"));
 
-    const path = require("path");
-
     app.get("*", (req, res) => {
-
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
     })
-
-
 }
 
-mongoose.connect("mongodb+srv://DeepakGunpal:hdg5NWwcvf2wUDTN@deepakcluster0.hynna.mongodb.net/dictionaryRoaDo", {
+mongoose.connect(process.env.DB, {
     useNewUrlParser: true
 }).then(
     _ => app.listen(PORT, (_ => console.log(`server is live on ${PORT}`)))
