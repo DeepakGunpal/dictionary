ef95ea823788749fe3d39ca784e2e6c72ca34092
Deepak <deepakgunpal0@gmail.com>
2022-08-16 13:54:22 +0530

    newgetapi

--- src/route/route.js
+++ src/route/route.js
@@ -3,7 +3,14 @@ const route = Router();
 import { addWord, getWord, word } from "../controller/word.js";
 
 route.get('/getWord/:word', getWord);
-route.get('/word/:word', word);
+route.post('/getWord/:word', getWord);
+route.post('/word/:word', word);
+route.get('/word1/:word', async (req, res) => {
+    res.json("i am working")
+});
+route.post('/word2/:word', async (req, res) => {
+    res.json("i am post")
+});
 
 route.post('/addWord/:word', addWord)
 
