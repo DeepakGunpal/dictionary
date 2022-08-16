7c994426902921af0a142059f4e1ee4685ee7dc1
Deepak <deepakgunpal0@gmail.com>
2022-08-16 20:47:09 +0530

    more styling

--- client/src/components/definations/Definations.js
+++ client/src/components/definations/Definations.js
@@ -7,49 +7,42 @@ const Definations = ({ word, meanings }) => {
             {word === "" ? (
                 <span className='subTitle'>Start by typing a word in Search</span>
             ) : (
-                meanings.map(def => (
-                    <div
-                        className='singleMean'
-                        style={{ backgroundColor: "white", color: "black" }}
-                    >
-                        <b style={{ backgroundColor: "white", color: "blue" }}>word : {def.title} </b>
-                        < hr />
+                meanings.length == 0 ? (<span className='subTitle'>Not available, Press Add word and Search again</span>) :
+                    (
+                        meanings.map(def => (
+                            <div
+                                className='singleMean'
+                                style={{ backgroundColor: "white", color: "black" }}
+                            >
+                                <b style={{ backgroundColor: "white", color: "blue", fontSize: '30px', textTransform: 'uppercase' }}>WORD : {def.title} </b>
+                                < hr />
+                                {
+                                    < span > <b >lexicalCategory : </b> {def.lexicalCategory} < hr /></span>
 
-                        <b >lexicalCategory : </b>
-                        {
-                            < span > {def.lexicalCategory} < hr /></span>
+                                }
 
-                        }
-                        <b >origin : </b>
-                        {
-                            < span > {def.origin} < hr /></span>
+                                {
+                                    < span > <b >origin : </b>{def.origin} < hr /></span>
 
-                        }
-                        <b>Definitions :</b> <hr />
-                        {
-                            def.definitions.map(defi => (
-                                <b> {defi} <hr /> </b>
-                            ))
-                        }
-                        <b>Example :</b> <hr />
-                        {
-                            def.example.map(ex => (
-                                <span>{ex} <hr /></span>
-                            ))
-                        }
-                        <b>Synonyms : </b>
-                        {
-                            def.synonyms.map(syn => (
-                                <span>{syn}, </span>
-                            ))
-                        }
-                        <br />
-                        <hr />
-                        <br />
+                                }
+                                <b>Definitions :</b> <hr />
+                                {
+                                    def.definitions.map(defi => (
+                                        <b> {defi} <hr /> </b>
+                                    ))
+                                }
+                                {def.example.length != 0 && (<b>Example : <hr /></b>)}
+                                {def.example.length != 0 && (
+                                    def.example.map(ex => (
+                                        <span>{ex} <hr /></span>
+                                    ))
+                                )}
+                                {def.synonyms.length != 0 && (<span> <b>Synonyms : </b> {def.synonyms.map(syn => `${syn}, `)}, </span>)}
 
-                    </div >
+                            </div >
 
-                ))
+                        ))
+                    )
             )
             }
         </div >
