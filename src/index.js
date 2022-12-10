590c54d18f9d9c7279b23730f9840c278648f6a0
Deepak <deepakgunpal0@gmail.com>
2022-12-10 17:28:33 +0530

    dictionary app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import route from './route/route.js';
-import path from "path";
+import cors from 'cors';
 import pkg from 'dotenv';
 pkg.config({ path: './src/config.env' });
 
@@ -10,23 +10,12 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 //cors
-app.use((req, res, next) => {
-    res.header({ "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type" });
-    next();
-});
+app.use(cors({
+    origin: "*"
+}));
 
-if (process.env.NODE_ENV == "production") {
-
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
-
-const DB = process.env.DB;
 // console.log(DB)
-mongoose.connect("mongodb+srv://DeepakGunpal:hdg5NWwcvf2wUDTN@deepakcluster0.hynna.mongodb.net/dictionaryRoaDo", {
+mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true
 }).then(
     _ => app.listen(PORT, (_ => console.log(`server is live on ${PORT}`)))
